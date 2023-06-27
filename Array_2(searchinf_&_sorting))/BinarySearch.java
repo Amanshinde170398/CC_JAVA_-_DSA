452fc1b4911700b33798e0d9847bcd426f9dcd6a
Aman Shinde <1703amanshinde98@gmail.com>
2023-06-28 01:01:15 +0530

    binary seacrh

--- Array_2(searchinf_&_sorting))/BinarySearch.java
+++ Array_2(searchinf_&_sorting))/BinarySearch.java
@@ -1,5 +1,25 @@
 class BinarySearch {
+
+    public static int binarySearch(int[] arr, int elem) {
+        int si = 0;
+        int ei = arr.length - 1;
+        int mid;
+        while (si <= ei) {
+            mid = (ei + si) / 2;
+            if (arr[mid] > elem) {
+                ei = mid - 1;
+            } else if (arr[mid] < elem) {
+                si = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        int[] arr = { 1, 3, 7, 9, 11, 12, 45 };
+        int index = binarySearch(arr, 45);
+        System.out.println(index);
     }
 }
